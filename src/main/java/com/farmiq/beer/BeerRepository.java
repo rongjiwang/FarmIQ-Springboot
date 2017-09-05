e9572e487e6a01565c84835814e2214490884247
Rongji Wang <rongjiwang@Rongjis-MacBook-Pro.local>
2017-09-05 20:04:18 +1200

    complete java backend integration with angular, need test with ms sql server

---
+++ src/main/java/com/farmiq/beer/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.farmiq.beer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+interface BeerRepository extends JpaRepository<Beer, Long>{
+
+}
